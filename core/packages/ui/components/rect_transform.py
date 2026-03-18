2668ff45a50df6fecec7aad8dd576762f94b7a37
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2026-03-18 15:16:43 +1100

    Add basic rect transform

--- core/packages/ui/components/rect_transform.py
+++ core/packages/ui/components/rect_transform.py
@@ -1,7 +1,27 @@
-"""A special type of Transform that also includes a width and height, as well as anchors."""
+"""A special type of Transform that represents the rectangle that a UIElement can be placed inside of."""
 
-from core.components.transform import Transform
+from core.packages.geometry.vector2 import Vector2
 
 
-class RectTransform(Transform):
-    """A special type of Transform that also includes a width and height, as well as anchors."""
+class RectTransform:
+    """A special type of Transform that represents the rectangle that a UIElement can be placed inside of."""
+
+    def __init__(  # noqa: PLR0913
+        self,
+        position: Vector2,
+        min_anchor: Vector2,
+        max_anchor: Vector2,
+        pivot: Vector2 | None = None,
+        rotation: float | None = None,
+        scale: Vector2 | None = None,
+    ) -> None:
+        """Initialize the RectTransform with a position."""
+        # rect stuff
+        self.min_anchor = min_anchor
+        self.max_anchor = max_anchor
+        self.pivot = pivot or Vector2(0.5, 0.5)
+
+        # transform stuff
+        self._local_position = position
+        self._local_rotation = rotation or 0
+        self._local_scale = scale or Vector2.zero()
