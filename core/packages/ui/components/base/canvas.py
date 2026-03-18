5d62622e46e845d934be2aac3cedceb5f760fdd3
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2026-03-18 14:50:52 +1100

    Add mode to canvas

--- core/packages/ui/components/base/canvas.py
+++ core/packages/ui/components/base/canvas.py
@@ -5,7 +5,7 @@ from enum import Enum, auto
 from core.packages.ui.components.ui_element import UIElement
 
 
-class CanvasRenderModes(Enum):
+class CanvasRenderMode(Enum):
     """How the canvas is rendered (part of the world or on top of it separately)."""
 
     WORLD_SPACE = auto()
@@ -15,8 +15,7 @@ class CanvasRenderModes(Enum):
 class Canvas(UIElement):
     """Basic Canvas component for the UI package."""
 
-    def __init__(
-        self,
-    ) -> None:
+    def __init__(self, *, mode: CanvasRenderMode = CanvasRenderMode.SCREEN_SPACE_OVERLAY) -> None:
         """Initialize the Canvas component."""
         super().__init__()
+        self.mode = mode
