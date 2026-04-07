f7623f4e13020fb61f640f8272b933aaf030efab
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2026-04-07 17:29:12 +1000

    Refactor RectTransform to use TransformBase

--- core/packages/ui/components/rect_transform.py
+++ core/packages/ui/components/rect_transform.py
@@ -1,27 +1,25 @@
 """A special type of Transform that represents the rectangle that a UIElement can be placed inside of."""
 
+from core.components.transform_base import TransformBase
 from core.packages.geometry.vector2 import Vector2
 
 
-class RectTransform:
+class RectTransform(TransformBase):
     """A special type of Transform that represents the rectangle that a UIElement can be placed inside of."""
 
     def __init__(  # noqa: PLR0913
         self,
-        position: Vector2,
         min_anchor: Vector2,
         max_anchor: Vector2,
         pivot: Vector2 | None = None,
+        position: Vector2 | None = None,
         rotation: float | None = None,
         scale: Vector2 | None = None,
     ) -> None:
         """Initialize the RectTransform with a position."""
+        super().__init__(local_position=position, local_rotation=rotation, local_scale=scale)
+
         # rect stuff
         self.min_anchor = min_anchor
         self.max_anchor = max_anchor
         self.pivot = pivot or Vector2(0.5, 0.5)
-
-        # transform stuff
-        self._local_position = position
-        self._local_rotation = rotation or 0
-        self._local_scale = scale or Vector2.zero()
