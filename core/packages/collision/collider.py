f8b52ae6303a975e65a200d6e864a8147bd12e6b
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2026-04-21 11:44:14 +1000

    Refactor collision manager

--- core/packages/collision/collider.py
+++ core/packages/collision/collider.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING, Literal, override
 
 from core.components.transform import Transform
 from core.packages.collision.collision_surface import DEFAULT_SURFACE, CollisionSurface
+from core.packages.physics.rigidbody_2d import RigidBody2D
 from game.behavior import Behavior
 from game.dirty import Dirtyable
 from game.error import MissingComponentDependencyError
@@ -28,6 +29,8 @@ class Collider(Behavior, Dirtyable):
         self.layers: set[CollisionLayer] = set()
 
         self.collision_surface = collision_surface or DEFAULT_SURFACE
+
+        self.rigid_body_2d: RigidBody2D | None = None
 
     @staticmethod
     def get_type() -> ColliderType:
@@ -52,3 +55,6 @@ class Collider(Behavior, Dirtyable):
             raise MissingComponentDependencyError(msg)
 
         self.transform = self.game_object.get_component(Transform)
+
+        if self.game_object.has_component(RigidBody2D):
+            self.rigid_body_2d = self.game_object.get_component(RigidBody2D)
--- core/packages/collision/collision_manager.py
+++ core/packages/collision/collision_manager.py
@@ -8,6 +8,7 @@ from typing import TYPE_CHECKING
 from core.packages.collision.collision import Collision
 from core.packages.collision.collision_detector import CollisionDetector
 from core.packages.collision.detectors.rect_rect import rect_rect_collision
+from core.packages.collision.upgrade import upgrade_collision
 
 if TYPE_CHECKING:
     from core.packages.collision.collider import Collider
@@ -53,7 +54,7 @@ class CollisionManager:
 
                 collision = collision_func(left, right)
                 if collision is not None:
-                    collisions.append(collision)
+                    collisions.append(upgrade_collision(collision))
 
         return collisions
 
@@ -82,56 +83,6 @@ class CollisionManager:
                 left.send_collision_event(collision, CollisionInteraction.EXIT)
                 right.send_collision_event(collision.get_inverse(), CollisionInteraction.EXIT)
 
-        self.last_frame_pairs = collider_pairs
-
-    def update(self) -> None:
-        """Update Collisions sending any Collision events to affected objects."""
-        collisions: list[Collision] = []
-
-        collider_pairs: set[tuple[Collider, Collider]] = set()
-
-        # e.g rule could be (Player can collide with Ground)
-        for rule in self.rules.values():
-            # e.g. [(Player, Ground1), (Player, Ground2)]
-            # these pairs are pre-filtered to only check what is necessary
-            pairs = rule.get_all_collider_pairs()
-
-            for pair in pairs:
-                collision_func = self.detector.get_detector(pair)
-
-                left = pair[0]
-                right = pair[1]
-
-                collision = collision_func(left, right)
-                if collision is not None:
-                    # send collision to both
-                    collisions.append(collision)
-                    # add collider pair to store for next frame
-                    collider_pairs.add((left, right))
-
-        # make sure we do the events after all collisions are handled (so we don't delete collisions etc.)
-        for collision in collisions:
-            left = collision.collider_a
-            right = collision.collider_b
-
-            collided_last_frame = (left, right) in self.last_frame_pairs
-            collision_type = CollisionInteraction.STAY if collided_last_frame else CollisionInteraction.ENTER
-
-            left.send_collision_event(collision, collision_type)
-            right.send_collision_event(collision.get_inverse(), collision_type)
-
-        # check for any exits from last frame's collisions
-        for pair in self.last_frame_pairs:
-            if pair not in collider_pairs:
-                left, right = pair
-
-                # no point Collision
-                collision = Collision(left, right, None)
-
-                left.send_collision_event(collision, CollisionInteraction.EXIT)
-                right.send_collision_event(collision.get_inverse(), CollisionInteraction.EXIT)
-
-        # reset collider pairs for next frame
         self.last_frame_pairs = collider_pairs
 
     def add_rule(self, rule: CollisionRule) -> None:
