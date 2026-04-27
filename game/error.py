fc2522f8c434dad67a614360d5936a085e5590ca
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2026-04-27 17:57:39 +1000

    Add not_implemented utility

--- game/error.py
+++ game/error.py
@@ -2,6 +2,24 @@
 
 from __future__ import annotations
 
+from types import MethodType
+from typing import Any
+
+
+def get_class_that_defined_method(method: MethodType) -> type[Any]:
+    """Get the class that defined a bound method."""
+    if not isinstance(method, MethodType):
+        msg = "Incorrect use 'not_implemented' or 'get_class_that_defined_method'. Expected a bound method."
+        raise TypeError(msg)
+    return type(method.__self__)
+
+
+def not_implemented(method: MethodType) -> None:
+    """Raise a not implemented error for a given class."""
+    cls = get_class_that_defined_method(method)
+    msg = f"'{cls.__name__}' does not implement the required abstract method '{method.__name__}'."
+    raise NotImplementedError(msg)
+
 
 class InvalidArgumentCombinationError(RuntimeError):
     """Exception raised when an illegal combination of arguments is used."""
