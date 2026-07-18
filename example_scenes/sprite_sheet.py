ba2ed080d5590bc0ecc428ef7bb16771b9fe8304
Ned Olsen <71749344+nedolsdev@users.noreply.github.com>
2026-07-18 12:17:56 +1000

    Update render layer import

--- example_scenes/sprite_sheet.py
+++ example_scenes/sprite_sheet.py
@@ -4,10 +4,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from core.components.render_layer import RenderLayer
 from core.components.transform import Transform
 from core.objects.empty import Empty
 from core.packages.geometry.vector2 import Vector2
+from core.packages.render.render_layer import RenderLayer
 from core.packages.spritesheet.sprite_sheet import SimpleSpriteSheet
 from example_scenes.helpers.camera_util import create_camera_game_object
 from example_scenes.helpers.test_sprite_sheet import get_sprite_sheet_grid
