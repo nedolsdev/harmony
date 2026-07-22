dc0e7143e77bf2f36e2fbb39bce60ff9af0fd063
Ned Olsen <71749344+nedolsdev@users.noreply.github.com>
2026-07-22 21:34:53 +1000

    Add more primitives

--- harmony/core/packages/render/primitive.py
+++ harmony/core/packages/render/primitive.py
@@ -8,7 +8,11 @@ from harmony.game.material import Material, NoMaterial
 
 if TYPE_CHECKING:
     from harmony.core.assets.sprite_image import SpriteImage
+    from harmony.core.packages.geometry.circle import Circle
     from harmony.core.packages.geometry.geometry import Geometry
+    from harmony.core.packages.geometry.line_segment import LineSegment
+    from harmony.core.packages.geometry.polygon import Polygon
+    from harmony.core.packages.geometry.rectangle import Rectangle
 
 
 class RenderPrimitive:
@@ -26,6 +30,41 @@ class GeometryRenderPrimitive(RenderPrimitive):
         """Initialize the GeometryRenderPrimitive."""
         super().__init__(material)
         self.geometry = geometry
+
+
+class Line2DRenderPrimitive(GeometryRenderPrimitive):
+    """A Line2D render primitive."""
+
+    def __init__(self, line_segment: LineSegment, material: Material | None = None) -> None:
+        """Initialize the Line2DRenderPrimitive."""
+        super().__init__(line_segment, material)
+
+
+class PolygonRenderPrimitive(GeometryRenderPrimitive):
+    """A Polygon render primitive."""
+
+    def __init__(self, polygon: Polygon, material: Material | None = None) -> None:
+        """Initialize the PolygonRenderPrimitive."""
+        super().__init__(polygon, material)
+        self.polygon: Polygon = polygon
+
+
+class RectRenderPrimitive(PolygonRenderPrimitive):
+    """A RectRenderPrimitive render primitive."""
+
+    def __init__(self, rect: Rectangle, material: Material | None = None) -> None:
+        """Initialize the RectRenderPrimitive."""
+        super().__init__(rect, material)
+        self.rect = rect
+
+
+class CircleRenderPrimitive(GeometryRenderPrimitive):
+    """A CircleRenderPrimitive render primitive."""
+
+    def __init__(self, circle: Circle, material: Material | None = None) -> None:
+        """Initialize the RectRenderPrimitive."""
+        super().__init__(circle, material)
+        self.circle = circle
 
 
 class SpriteRenderPrimitive(RenderPrimitive):
